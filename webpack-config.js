ae78db236f2ab5d3bdccf5e779da80da1d0da4f2
Ark2307 <kr.aryan2307@gmail.com>
2024-05-30 15:10:53 +0530

    added webpack

---
+++ webpack-config.js
@@ -0,0 +1,115 @@
+var path = require('path')
+var webpack = require('webpack')
+require("babel-polyfill")
+const TerserPlugin = require("terser-webpack-plugin");
+
+function resolve (dir) {
+      return path.join(__dirname, '..', dir)
+}
+
+function hashGenerator() {
+  return 1
+}
+
+process.env.HASH = hashGenerator()
+
+module.exports = {
+  entry: {"babel-polyfill": "babel-polyfill", main: './src/index.js',},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'https://d1hvi6xs55woen.cloudfront.net/dependency-graph/dist/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+              // sass-loader version >= 8
+              sassOptions: {
+                indentedSyntax: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+             ["@babel/preset-react", {"runtime": "automatic"}]
+          ]
+          }
+        }
+
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        type:"asset"
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': resolve('/src')
+    },
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: 'eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = 'source-map'
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new TerserPlugin({
+      "terserOptions":{
+        compress:true,
+        sourceMap: true,
+        output:{
+          ascii_only:true
+        }
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+  ])
+}
